e061204adede8acd67a758b468c678a533102269
Hichem <hichembouzourine.uni@gmail.com>
2022-07-26 15:12:18 +0100

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import NavBar from "./components/common/navBar";
 import Movies from "./components/movies";
+import Customers from "./components/common/customers";
+import Rentals from "./components/common/rentals";
+import NotFound from "./components/common/notFound";
+import "./App.css";
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Navigate to="movies" />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="rentals" element={<Rentals />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </main>
   );
 }
